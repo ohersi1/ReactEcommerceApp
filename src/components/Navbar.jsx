f47bab4bf2e8f38875a8ecfdd90ec55e9360be90
Osman Hersi <osmanhersi@hotmail.co.uk>
2025-09-25 01:49:00 +0100

    fixed navbar added hamburger menu and tested it changed hero picture to better quality one as well

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,27 +1,11 @@
-import React from 'react';
-// import { Link } from 'react-router-dom';
-
-
-
-// const NavBar = () => {
-//   return (
-//     <div className='navbar'>
-//       <ul>
-//         <li><Link to="/" >Home</Link></li>
-//         <li><Link to="/products" >Products</Link></li>
-//         <li><Link to="cart" >Cart</Link></li>
-//         <li><Link to="/checkout" >Checkout</Link></li>
-//       </ul>
-//     </div>
-//   )
-// }
-
-// export default NavBar
-
+import React from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
-import logo from "../assets/logo.png"; // <-- save your image into src/assets
-import './Navbar.css';
+import logo from "../assets/logo.png";
+import "./Navbar.css";
 const NavBar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <nav className="navbar">
       <div className="navbar-left">
@@ -29,16 +13,27 @@ const NavBar = () => {
           <img src={logo} alt="RetroVault Logo" className="navbar-logo" />
         </Link>
       </div>
-      <ul className="navbar-links">
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/products">Products</Link></li>
-        <li><Link to="/cart">Cart</Link></li>
-        <li><Link to="/checkout">Checkout</Link></li>
+      <ul className={`navbar-links ${menuOpen ? "active" : ""}`}>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/products">Products</Link>
+        </li>
+        <li>
+          <Link to="/cart">Cart</Link>
+        </li>
+        <li>
+          <Link to="/checkout">Checkout</Link>
+        </li>
       </ul>
-      <div className="hamburger">
-        <span className='bar'></span>
-        <span className='bar'></span>
-        <span className='bar'></span>
+      <div
+        className={`hamburger ${menuOpen ? "active" : ""}`}
+        onClick={() => setMenuOpen(!menuOpen)}
+      >
+        <span className="bar"></span>
+        <span className="bar"></span>
+        <span className="bar"></span>
       </div>
     </nav>
   );
