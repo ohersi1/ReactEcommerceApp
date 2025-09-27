96f6ca46ad497c697ef7be2bdae712b10464ce52
Osman Hersi <osmanhersi@hotmail.co.uk>
2025-09-27 02:54:41 +0100

    improved the cart and added totals and added quantity buttons

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,18 +1,21 @@
 import React from "react";
 const CartItem = ({ items }) => {
+    let total = 0;
   return (
     <div>
-      Cart Item
+      Cart Items
       {items.map((item, i) => {
+        total+= item.price * item.quantity;
         return (
           <div key={i}>
             <h2>{item.name}</h2>
             <p>£{item.price}</p>
             <p>{item.quantity}</p>
-            <p>Total: £{item.price * item.quantity}</p>
+            <p>{item.quantity * item.price}</p>
           </div>
         );
       })}
+      <div>£{total}</div>
     </div>
   );
 };
--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -7,6 +7,21 @@ const CartPage = () => {
     {
       name: "PS2",
       price: 149.99,
+      quantity: 1,
+    },
+    {
+      name: "Nintendo Wii",
+      price: 299.99,
+      quantity: 4,
+    },
+    {
+      name: "Nintendo 64",
+      price: 99.99,
+      quantity: 5,
+    },
+    {
+      name: "Sony Ericsson W810i",
+      price: 349.99,
       quantity: 2,
     },
   ]);
